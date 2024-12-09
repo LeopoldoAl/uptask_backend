b5105ed4809b6af1a8405528628e0911c78aa4e2
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-12-09 14:01:39 -0400

    Stage 9

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+import colors from 'colors'
 import server from './server'
 
 const port = process.env.PORT || 3000
 
 server.listen(port, () => {
-    console.log(`REST API working at the port ${port}`)
+    console.log(colors.cyan.bold(`REST API working at the port ${port}`))
 })
