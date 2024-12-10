3bcedf9cc656d4078ddcec36d936d083551506e0
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-12-10 11:10:02 -0400

    Stage 15

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,4 +1,4 @@
-import { Document, Schema } from "mongoose";
+import mongoose, { Document, Schema } from "mongoose"
 
 export type ProjectType = Document & {
     projectNmae: string
@@ -10,7 +10,19 @@ const ProjectSchema :  Schema = new Schema({
     projectName: {
         type: String,
         requiree: true,
-        trim: true,
-        unique: true
-    }
+        trim: true
+    },
+    clientName: {
+        type: String,
+        requiree: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        requiree: true,
+        trim: true
+    },
 })
+
+const Project = mongoose.model<ProjectType>('Project', ProjectSchema)
+export default Project
