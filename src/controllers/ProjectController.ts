7562987293b2cd44859be0dc2add3d424ea2db70
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-12-10 13:27:29 -0400

    Stage 23

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -2,8 +2,10 @@ import { Request, Response } from "express"
 import Project from "../models/Project"
 export class ProjectController {
     static createProject = async (req: Request, res: Response) => {
+        const project = new Project(req.body)
+        
         try {
-            await Project.create(req.body)
+            await project.save()
             res.send('Project has been created successfully!')   
         } catch (error) {
             console.log(error)
