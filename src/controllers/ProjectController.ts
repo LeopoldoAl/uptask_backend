07b369fcaaaa0a9d8dc82971211338ab9387efba
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-12-10 12:34:19 -0400

    Stage 16

---
+++ src/controllers/ProjectController.ts
@@ -0,0 +1,6 @@
+import { Request, Response } from "express"
+export class ProjectController {
+    static getAllProjects = async (req: Request, res: Response) => {
+        res.send('All of the projects')       
+    }
+}
