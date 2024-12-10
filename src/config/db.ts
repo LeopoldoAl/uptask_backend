ecba8256b4d7346f89100e0aaa5ddbb23a8b5677
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-12-10 10:42:50 -0400

    Stage 13

--- src/config/db.ts
+++ src/config/db.ts
@@ -5,13 +5,13 @@ import { exit } from 'node:process';
 export const connectDB = async () => {
     try {
         
-        const connection = await mongoose.connect(process.env.DATABASE_URL)
-        const utl = `${connection.connection.host}:${connection.connection.port}`
+        const {connection} = await mongoose.connect(process.env.DATABASE_URL)
+        const utl = `${connection.host}:${connection.port}`
         console.log(colors.magenta.bold(`MongoDB connected on: ${utl}`))
         
         
     } catch (error) {
-        console.log(error.message)
+        console.log(colors.red.bold('Error during the connection to MongoDB'))
         exit(1)
     }
 }
