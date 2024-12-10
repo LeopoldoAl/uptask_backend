baab53e386c67d2e72e0c2731cfd735a5b2d2e4f
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-12-10 10:30:08 -0400

    Stage 12

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,14 +1,17 @@
+import colors from "colors";
 import mongoose from "mongoose"
+import { exit } from 'node:process';
 
 export const connectDB = async () => {
     try {
         
         const connection = await mongoose.connect(process.env.DATABASE_URL)
-
-        console.log(connection)
+        const utl = `${connection.connection.host}:${connection.connection.port}`
+        console.log(colors.magenta.bold(`MongoDB connected on: ${utl}`))
+        
         
     } catch (error) {
         console.log(error.message)
-        process.exit(1)
+        exit(1)
     }
 }
