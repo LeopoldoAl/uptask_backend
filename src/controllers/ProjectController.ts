0a50bcf3b5435118fca0ffcd6fc4aa6033acec5b
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-12-11 09:58:31 -0400

    Stage 28

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -14,7 +14,7 @@ export class ProjectController {
     
     static getAllProjects = async (req: Request, res: Response) => {
        try {
-        const projects = await Project.find({})
+        const projects = await Project.find()
          res.json(projects)   
        } catch (error) {
         console.log(error)
