fc25a5c4755ae447e7e31262ae72979e333ac57d
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-12-11 11:49:00 -0400

    Stage 31

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -3,37 +3,58 @@ import Project from "../models/Project"
 export class ProjectController {
     static createProject = async (req: Request, res: Response) => {
         const project = new Project(req.body)
-        
+
         try {
             await project.save()
-            res.send('Project has been created successfully!')   
+            res.send('Project has been created successfully!')
         } catch (error) {
             console.log(error)
-        }    
+        }
     }
-    
+
     static getAllProjects = async (req: Request, res: Response) => {
-       try {
-        const projects = await Project.find()
-         res.json(projects)   
-       } catch (error) {
-        console.log(error)
-       }    
+        try {
+            const projects = await Project.find()
+            res.json(projects)
+        } catch (error) {
+            console.log(error)
+        }
     }
     static getProjectById = async (req: Request, res: Response) => {
-        const {id} = req.params
-       try {
-        const project = await Project.findById(id)
-         if (!project) {
-            const error = new Error("Project did not find!")
-            res.status(404).json({
-                error: error.message
-            })
-            return 
-         }
-         res.json(project)
-       } catch (error) {
-        console.log(error)
-       }    
+        const { id } = req.params
+        try {
+            const project = await Project.findById(id)
+            if (!project) {
+                const error = new Error("Project did not find!")
+                res.status(404).json({
+                    error: error.message
+                })
+                return
+            }
+            res.json(project)
+        } catch (error) {
+            console.log(error)
+        }
     }
+
+    static updateProduct = async (req: Request, res: Response) => {
+        const { id } = req.params
+        try {
+            const project = await Project.findByIdAndUpdate(id, req.body)
+
+            if (!project) {
+                const error = new Error("Project did not find!")
+                res.status(404).json({
+                    error: error.message
+                })
+                return
+            }
+            await project.save()
+            res.send('Project has been updated correctly!')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
 }
