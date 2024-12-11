d95b8b32a23aa1a87f776c47c47232236a35ee80
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-12-11 09:40:48 -0400

    Stage 26

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,9 +1,21 @@
 import { Router } from "express"
 import { ProjectController } from "../controllers/ProjectController"
+import { body } from "express-validator"
+import { handleInputErrors } from "../middleware/validation"
 
 const router = Router()
 
-router.post('/', ProjectController.createProject)
+router.post('/', 
+    body("projectName")
+        .notEmpty().withMessage("The project name is obligatory!"),
+    body("clientName")
+        .notEmpty().withMessage("The client name is obligatory!"),
+    body("description")
+        .notEmpty().withMessage("The description is obligatory!"),
+    // Here it goes the middleware
+    handleInputErrors,
+    ProjectController.createProject
+)
 router.get('/', ProjectController.getAllProjects)
 
 export default router
