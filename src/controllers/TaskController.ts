638b464a1fa1839bb721428059d5a2f638e88493
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-12-12 11:16:41 -0400

    Stage 42

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -1,15 +1,29 @@
 import { Request, Response } from "express"
+import Project from "../models/Project"
+import Task from "../models/Task"
 
 export class TaskController {
-    static createProject = async (req: Request, res: Response) => {
+    static createTask = async (req: Request, res: Response) => {
         const {projectId} = req.params
-        console.log(projectId);
+        const project = await Project.findById(projectId)
+        if (!project) {
+            const error = new Error("Project did not find!")
+            res.status(404).json({
+                error: error.message
+            })
+            return
+        }
         
         try {
-            
+            const task = new Task(req.body)
+            task.project = project.id
+            project.tasks.push(task.id)
+            await task.save()
+            await project.save()
+            res.send('Task created correctly!')
         } catch (error) {
             console.log(error)
             
         }
     }
-}
+}   
