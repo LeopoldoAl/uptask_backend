a92c7f9901202897005f61e0c9d92e4adc608457
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-12-24 11:24:31 -0400

    Stage 76

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from 'dotenv'
 import cors from 'cors'
+import morgan from "morgan"
 import { corsConfig } from "./config/cors"
 import { connectDB } from "./config/db"
 import projectRoutes from "./routes/projectRoutes"
@@ -12,7 +13,12 @@ connectDB()
 const app = express()
 app.use(cors(corsConfig))
 
+// Logging
+app.use(morgan('dev'))
+
+// Reading data from the form
 app.use(express.json())
+
 // Routes
 app.use('/api/projects', projectRoutes)
 
