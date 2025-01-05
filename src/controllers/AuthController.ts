b15880491b6a5816c7e5d0c962d0e816d4c274fb
Leopoldo <leopoldoalvaradogomez@gmail.com>
2025-01-05 14:38:21 -0400

    Stage 84

---
+++ src/controllers/AuthController.ts
@@ -0,0 +1,6 @@
+import { Request, Response } from "express"
+    export class AuthController {
+        static createAccount = async (req: Request, res: Response) => {
+            res.send('From /api/auth')
+        }
+    }
