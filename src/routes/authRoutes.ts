4e266c19175aff708e771d88e32f526c7ec1f2fb
Leopoldo <leopoldoalvaradogomez@gmail.com>
2025-01-05 14:38:34 -0400

    Stage 85

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,26 @@
 import { Router } from "express"
+import { AuthController } from "../controllers/AuthController"
+import { body } from "express-validator"
+import { handleInputErrors } from "../middleware/validation"
 
 const router = Router()
-router.get('/', (req, res) =>{
-    res.send('From /api/auth')
-})
+router.post('/create-account',
+    body('name')
+        .notEmpty().withMessage("The name can not go empty!"),
+    body('password')
+        .isLength({ min: 8 })
+        .withMessage("The password is too short, as at least it should have 8 characters"),
+    body('password_confirmation').custom((value, {req}) =>{
+        if (value !== req.body.password) {
+            throw new Error("Password fields are not equal!")
+        }
+        return true
+    }),
+    body('email')
+        .isEmail().withMessage("Email doesn't valid!"),
+    handleInputErrors,
+    AuthController.createAccount
+)
 
 
 export default router
