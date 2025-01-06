64a5bd1a4ab6f17d5acf6a701dc22209def4bd32
Leopoldo <leopoldoalvaradogomez@gmail.com>
2025-01-06 12:45:47 -0400

    Stage 89

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,9 +1,13 @@
 import { Request, Response } from "express"
 import User from "../models/User"
+import { hashPassword } from "../utils/auth"
     export class AuthController {
         static createAccount = async (req: Request, res: Response) => {
             try {
+                const { password }  = req.body
                 const user = new User(req.body)
+                // Hash password
+                user.password = await hashPassword(password)
                 await user.save()
                 res.send('Account has been created, review your email to confirm!')
             } catch (error) {
