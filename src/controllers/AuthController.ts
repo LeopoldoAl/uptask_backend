a9cdf367073c61f4d5191d008c76bc198ad64b73
Leopoldo <leopoldoalvaradogomez@gmail.com>
2025-01-07 11:10:51 -0400

    Stage 99

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -35,4 +35,17 @@ import { AuthEmail } from "../emails/AuthEmail"
                 res.status(500).json({error: 'There was an error!'})
             }
         }
+        static confirmAccount = async (req: Request, res: Response) => {
+            try {
+                const { token } = req.body
+                const tokenExists = await Token.findOne({token})
+                if (!tokenExists) {
+                    const error = new Error("Token does no valid!")
+                    res.status(401).json({error: error.message})
+                    return
+                }
+            } catch (error) {
+                res.status(500).json({error: 'There was an error!'})
+            }
+        }
     }
