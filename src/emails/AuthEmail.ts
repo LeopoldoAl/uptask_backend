786698f9b3df1efa6623a46e00806168ff609c4c
Leopoldo <leopoldoalvaradogomez@gmail.com>
2025-01-07 10:28:17 -0400

    Stage 97

---
+++ src/emails/AuthEmail.ts
@@ -0,0 +1,23 @@
+import { transport } from "../config/nodemailer"
+interface IEmail {
+    email: string,
+    name: string,
+    token: string
+}
+export class AuthEmail {
+    static sendConfirmationEmail = async (user: IEmail) => {
+        const info = await transport.sendMail({
+            from: 'UpTask <admin@uptask.com>',
+            to: user.email,
+            subject: 'UpTask - Confirm your account',
+            text: 'UpTask - Confirm your account',
+            html: `<p>Hello ${user.name}, has created your account in UpTask, are all almost ready, you should only confirm your account</p>
+            <p>Visit the following link</p>
+            <a href="">Confirm account</a>
+            <p>Ingress the code: <b>${user.token}</b></p>
+            <p>This token will expire in 10 minutes</p>
+            `
+        })
+        console.log('Message sent',info.messageId)
+    }
+}
