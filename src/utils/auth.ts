ce989fe279d8af5ef759cfdaf1c0a4f56cd23752
Leopoldo <leopoldoalvaradogomez@gmail.com>
2025-01-08 09:44:19 -0400

    Stage 105

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,6 +1,10 @@
-import { genSalt, hash } from "bcrypt"
+import { compare, genSalt, hash } from "bcrypt"
 
 export const hashPassword = async (password: string) => {
     const salt = await genSalt(10)
     return await hash(password, salt)
+}
+
+export const checkPassword = async (enteredPassword:string, storeHash: string): Promise<boolean> => {
+    return await compare(enteredPassword, storeHash)
 }
