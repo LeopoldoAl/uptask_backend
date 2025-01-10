76dfc52a44b3b484c50c041fb5fa3f05f2f5907d
Leopoldo <leopoldoalvaradogomez@gmail.com>
2025-01-10 10:58:30 -0400

    Stage 111

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -124,4 +124,31 @@ export class AuthController {
             res.status(500).json({ error: 'There was an error!' })
         }
     }
+    static forgotPassword = async (req: Request, res: Response) => {
+        try {
+            const { email } = req.body
+            // Searching that the user exists
+            const user = await User.findOne({ email })
+            if (!user) {
+                const error = new Error('This user is not registered!')
+                res.status(404).json({ error: error.message })
+                return
+            }
+            
+            // Generating Token
+            const token = new Token()
+            token.token = generateToken()
+            token.user = user.id
+            await token.save()
+            // Sending email
+            AuthEmail.sendPasswordResetToken({
+                email: user.email,
+                name: user.name,
+                token: token.token
+            })
+            res.send('Review your email to receive the instructions')
+        } catch (error) {
+            res.status(500).json({ error: 'There was an error!' })
+        }
+    }
 }
