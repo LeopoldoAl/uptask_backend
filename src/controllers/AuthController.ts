f9793a2b01bfc2d6651a11a3cef92af1cd4abeb5
Leopoldo <leopoldoalvaradogomez@gmail.com>
2025-01-12 12:41:53 -0400

    Stage 115

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -165,4 +165,22 @@ export class AuthController {
             res.status(500).json({ error: 'There was an error!' })
         }
     }
+    static updatePasswordWithToken = async (req: Request, res: Response) => {
+        try {
+            const { token } = req.params
+            const { password } = req.body
+            const tokenExists = await Token.findOne({ token })
+            if (!tokenExists) {
+                const error = new Error("Token does not valid!")
+                res.status(404).json({ error: error.message })
+                return
+            }
+            const user = await User.findById(tokenExists.user)
+            user.password = await hashPassword(password)
+            await Promise.allSettled([user.save(), tokenExists.deleteOne()])
+            res.send('The password was modified correctly!')
+        } catch (error) {
+            res.status(500).json({ error: 'There was an error!' })
+        }
+    }
 }
