cc3d009f770702edf9b330737055cb09d5b2c31e
Leopoldo <leopoldoalvaradogomez@gmail.com>
2025-01-14 09:40:17 -0400

    Stage 124

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -6,7 +6,7 @@ type UserPayload = {
 export const generateJWT = (payload: UserPayload) => {
     
     const token = jwt.sign(payload, process.env.JWT_SECRET, {
-        expiresIn: '6m', // 6 moths
+        expiresIn: '180d', // 6 moths
     })
     return token
 }
