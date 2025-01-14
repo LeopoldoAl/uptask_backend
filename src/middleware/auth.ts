6f56ca29e0cfbc43d2c09285edf41484810d182e
Leopoldo <leopoldoalvaradogomez@gmail.com>
2025-01-14 09:40:27 -0400

    Stage 125

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from "express"
+import jwt from 'jsonwebtoken'
+import User from "../models/User"
 export const authenticate = async (req:Request, res: Response, next: NextFunction) => {
     const bearer = req.headers.authorization
     if(!bearer) {
@@ -7,6 +9,15 @@ export const authenticate = async (req:Request, res: Response, next: NextFunctio
         return
     }
     const [,token] = bearer.split(' ')
-    console.log(token)
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        if (typeof decoded === 'object' && decoded.id) {
+            const user = await User.findById(decoded.id)
+            console.log(user)
+        }
+        
+    } catch (error) {
+        res.status(500).json({error: 'Token does not valid'})
+    }
     next()
 }
