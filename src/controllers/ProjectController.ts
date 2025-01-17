a1d3bee591d7e36013c40c01f5d673064b1bd425
Leopoldo <leopoldoalvaradogomez@gmail.com>
2025-01-17 12:05:23 -0400

    Stage 140

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -17,7 +17,8 @@ export class ProjectController {
         try {
             const projects = await Project.find({
                 $or: [
-                    {manager: {$in: req.user.id}}
+                    {manager: {$in: req.user.id}},
+                    {team: {$in: req.user.id}}
                 ]
             })
             res.json(projects)
@@ -36,7 +37,7 @@ export class ProjectController {
                 })
                 return
             }
-            if (project.manager.toString() !== req.user.id.toString()) {
+            if (project.manager.toString() !== req.user.id.toString() && !project.team.includes(req.user.id)) {
                 const error = new Error("Action doesn't valid!")
                 res.status(404).json({
                     error: error.message
