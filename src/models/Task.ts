6e98344b7110115400cae8d2aa2cb3e23d0eebac
Leopoldo <leopoldoalvaradogomez@gmail.com>
2025-01-28 10:44:55 -0400

    Stage 155

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,4 +1,5 @@
 import mongoose, { Document, Schema, Types } from "mongoose"
+import Note from "./Note"
 const taskStatus = {
     PENDING: 'pending',
     ON_HOLD: 'onHold',
@@ -60,5 +61,11 @@ export const TaskSchema: Schema = new Schema({
         }
     ]
 }, { timestamps: true })
+// Middleware
+TaskSchema.pre('deleteOne',{document: true}, async function() {
+    const taskId = this._id
+    if(!taskId) return
+    await Note.deleteMany({task: taskId})
+})
 const Task = mongoose.model<ITask>('Task', TaskSchema)
 export default Task
